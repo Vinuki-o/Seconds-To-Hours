9869c566927f005eb6f606df5bda6e80cc2d6138
harsha0771 <vinukiomalshara00@gmail.com>
2024-06-10 13:01:21 +0530

    init

---
+++ SecondsToHours/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondsToHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Enter the time in seconds: ");
+            int seconds =Convert.ToInt32(Console.ReadLine());
+
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600)/60;
+            int RemainSeconds = seconds % 60;
+
+            Console.WriteLine(seconds + "Seconds is " + hours  + "hours, " + minutes  + "minutes, " + RemainSeconds  + "seconds.");
+
+        }
+    }
+}
